abf7489086e35f737275fdaa7834351447cb23e6
Phat <ntphat2@orangetheory.com>
2021-11-02 17:05:14 +0700

    add to husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
 import { NextPage, NextPageContext } from "next";
 import Header from "../components/Header";
 
-export async function getStaticProps(context: NextPageContext) {
-  return {
-    props: {
-      env: process.env.NEXT_ENV
-    },
-  }
-}
-
-const Home: NextPage<{ env: string }> = ({ env }) => {
+const Home: NextPage = ({ env }) => {
   return (
     <>
       <Header />
@@ -31,7 +23,6 @@ const Home: NextPage<{ env: string }> = ({ env }) => {
         corporis sit assumenda dolor fugiat, quibusdam cupiditate hic, mollitia
         nihil.
       </h1>
-      <h1 className="text-5xl text-red-700">ENV {env}</h1>
       <h1>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque possimus
         quaerat inventore. Unde fugit officiis enim! Dolore eius ducimus porro
