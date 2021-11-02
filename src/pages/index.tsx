8bb7723071262b5256375551e6d72410642c6eee
Phat <ntphat2@orangetheory.com>
2021-11-02 17:14:12 +0700

    test husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,6 +53,12 @@ const Home: NextPage = ({ env }) => {
         corporis sit assumenda dolor fugiat, quibusdam cupiditate hic, mollitia
         nihil.
       </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque possimus
+        quaerat inventore. Unde fugit officiis enim! Dolore eius ducimus porro
+        corporis sit assumenda dolor fugiat, quibusdam cupiditate hic, mollitia
+        nihil.
+      </h1>
     </>
   );
 };
